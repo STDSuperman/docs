425cb9e0144b97a6e5cb2097cc310fe4a7ad1ba9
STDSuperman <2750556766@qq.com>
2020-10-27 10:05:03 +0800

    修改首页，文件结构名

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,15 +1,16 @@
 module.exports = [
     { text: '首页', link: '/' },
-    { text: '前端', link: '/FE/', items: [
-        { text: '前端文章', items: [
+    { text: '前端', link: '/pages/7b1e86/', items: [
+        { text: '前端文章', link: '/pages/7b1e86/', items: [
             { text: '面试相关', link: '/pages/7b1e86/'},
             { text: 'Vue', link: '/pages/cae655/'},
             { text: 'Electron', link: '/pages/7d9966/'},
         ]}
     ]},
-    { text: '时光漫谈', link: '/时光漫谈/', items: [
+    { text: '时光漫谈', link: '/pages/4f099a/', items: [
         { text: '我的秋招之旅', link: '/pages/4f099a/'},
         { text: '2019年终总结', link: '/pages/50b2d2/'},
     ]},
+    { text: '收藏夹', link: '/bookmark/' },
     { text: '关于', link: '/about/' }
 ]
---
+++ docs/bookmark/README.md
@@ -0,0 +1,10 @@
+---
+title: 收藏夹
+date: 2020-10-27 09:45:07
+permalink: /pages/3c2582/
+categories:
+  - bookmark
+tags:
+  - 
+---
+[花裤衩大佬收藏夹](https://panjiachen.github.io/awesome-bookmarks/)
